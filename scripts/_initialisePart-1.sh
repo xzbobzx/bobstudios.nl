0e9e2255252e031d67099dd6e34a39e5ee920365
James Harris <wopiancitizen123@gmail.com>
2016-01-27 10:01:59 +0000

    Update _initialisePart-1.sh

--- scripts/_initialisePart-1.sh
+++ scripts/_initialisePart-1.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 # File name: _initialisePart-1.sh
 
-source ./config.conf
-
-cd $location
-
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 
 echo -e "Close this window and open '_initialisePart-2'"
